ab0d02796f2ed765de333f53e49ab10e2b582109
Shanmukha <rohitgusain792@gmail.com>
2024-07-04 22:44:36 +0530

    Update IncreasingArraySize.cpp

--- 2_ArrayRepresentations/IncreasingArraySize.cpp
+++ 2_ArrayRepresentations/IncreasingArraySize.cpp
@@ -1,15 +1,33 @@
 #include <iostream>
 int main(){
-    int *p,*q;
-    int i;
-    q=(int*)malloc(sizeof(int)*10);
-    for(i=0;i<5;i++){
-        q[i]=p[i];
-        }
-    free(p);
-    p=q;
-    q=NULL;
-    for(i=0;i<5;i++){
-        printf("%d \n",p[i]);
+    int *p, *q;
+    int i; 
+    
+   
+    q = new int[10];
+    
+    
+    p = new int[5]{3, 5, 8, 10, 12};
+
+   
+    for(i=0; i<5; i++){
+        q[i] = p[i];
+    }
+    
+    
+    delete[] p;
+    
+   
+    p = q;
+    q = NULL;
+    
+    
+    for(i=0; i<5; i++){
+        std::cout << p[i] << std::endl;
+    }
+    
+   
+    delete[] p;
+
     return 0;
-}}
+}
