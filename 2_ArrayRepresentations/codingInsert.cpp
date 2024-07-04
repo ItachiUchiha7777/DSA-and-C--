67ec2486541e41bc192fa81ebcb37303581d2b15
Shanmukha <rohitgusain792@gmail.com>
2024-07-05 00:19:22 +0530

    Inserting array ADT

--- 2_ArrayRepresentations/codingInsert.cpp
+++ 2_ArrayRepresentations/codingInsert.cpp
@@ -9,14 +9,24 @@ struct Array
 };
 void Display(struct Array  arr){
     int i;
-    printf("\nElement are \n:");
+    printf("\nElement are:\n");
     for(int i=0;i<arr.length;i++){
         printf("%d ",arr.A[i]);
     }
 }
-
+void Insert(struct Array *arr,int index, int x){
+    if (index>=0 && index<=arr->length){
+    for( int i=arr->length;i>index;i--){
+        arr->A[i] = arr->A[i-1];
+    }
+    arr->A[index]=x;
+        arr->length++; 
+    }    
+}
 
 
 int main(){
-
+struct Array arr={{2,3,4,5,6},10,5};
+Insert(&arr,5,10);
+Display(arr);
 }
