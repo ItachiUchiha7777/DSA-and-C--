2011bc609c64b7b19ea379e5cf04b2a2898f37bc
Shanmukha <rohitgusain792@gmail.com>
2024-07-07 23:23:16 +0530

    Binary Search

--- 2_ArrayRepresentations/ArrayADT.cpp
+++ 2_ArrayRepresentations/ArrayADT.cpp
@@ -39,10 +39,28 @@ int Delete(struct Array *arr,int index )
     return 0;
 };
 
+void swap(int *x, int *y){
+    int temp=*x;
+    *x=*y;
+    *y=temp;
+}
+
+int LinearSearcher(struct  Array *arr,int key){
+int i;
+for(int i=0;i<arr->length;i++){
+    if(key==arr->A[i]){
+        swap(&arr->A[i],&arr->A[i+1]);
+        return i;
+    }
+}
+return -1;
+}
+
 
 int main(){
 struct Array arr={{2,3,4,5,6},10,5};
 Insert(&arr,5,10);
 Delete(&arr,0);
+
 Display(arr);
 }
