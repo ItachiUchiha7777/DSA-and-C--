e00ca3b6d3f7ba4bf361116f369a039ffafb87f1
Shanmukha <rohitgusain792@gmail.com>
2024-07-26 17:50:39 +0530

    leetcode 1518

---
+++ LeetCode/1518. Water Bottles.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def numWaterBottles(self, numBottles, numExchange):
+        """
+        :type numBottles: int
+        :type numExchange: int
+        :rtype: int
+        """
+        c=numBottles
+        while numBottles>=numExchange:
+            div=numBottles//numExchange
+            rem=numBottles%numExchange
+            numBottles=div+rem
+            c+=div
+
+        return c
+            
+           
+           
